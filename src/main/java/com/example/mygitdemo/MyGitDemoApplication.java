08168bcd5a737f106407345f054835f5ef22d680
Gavin <1524018075@qq.com>
2024-01-05 13:00:42 +0800

    Demo

---
+++ src/main/java/com/example/mygitdemo/MyGitDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mygitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGitDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyGitDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mygitdemo/MyGitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mygitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGitDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
