73f1bd7870b4b8dc60590aa4f40118ef2254d6f2
Gavin <1524018075@qq.com>
2024-01-05 13:02:24 +0800

    Demo

---
+++ src/main/java/com/example/mygitdemo/hahaah/oooooo.java
@@ -0,0 +1,9 @@
+package com.example.mygitdemo.hahaah;
+
+public class oooooo {
+    public static void main(String[] args) {
+        int i=1;
+        int m=i/0;
+        System.out.println("dasdsadasfsa");
+    }
+}
